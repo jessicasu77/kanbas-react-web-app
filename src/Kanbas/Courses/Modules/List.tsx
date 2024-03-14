af9150ea9b878785071689b78fe039fa7bd514e3
jessicasu77 <jessica.su0807@gmail.com>
2024-03-14 13:15:14 -0400

    modules page wip | up to module reducer 3.34

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -6,8 +6,60 @@ import { useParams } from "react-router";
 
 function ModuleList() {
   const { courseId } = useParams();
-  const modulesList = modules.filter((module) => module.course === courseId);
-  const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+  const [moduleList, setModuleList] = useState<any[]>(modules);
+  const [module, setModule] = useState({
+    _id: "placeholder",
+    name: "New Module",
+    description: "New Description",
+    course: courseId,
+  });
+  const deleteModule = (moduleId: string) => {
+    const newModuleList = moduleList.filter(
+      (module) => module._id !== moduleId );
+    setModuleList(newModuleList);
+  };
+
+  const AddNewModule = () => {
+    const addModule = (module: any) => {
+      const newModule = { ...module,
+        _id: new Date().getTime().toString() };
+      const newModuleList = [newModule, ...moduleList];
+      setModuleList(newModuleList);
+    };
+    const updateModule = () => {
+      const newModuleList = moduleList.map((m) => {
+        if (m._id === module._id) {
+          return module;
+        } else {
+          return m;
+        }
+      });
+      setModuleList(newModuleList);
+    };
+    return (
+      <div className="add-new-module">
+        <h3>Add a New Module</h3>
+        <input value={module.name} className="form-control"
+          onChange={(e) => setModule({
+            ...module, name: e.target.value })}
+        />
+        <br/>
+        <textarea value={module.description} className="form-control"
+          onChange={(e) => setModule({
+            ...module, description: e.target.value })}
+        />
+        <br/>
+        <button onClick={() => { addModule(module) }} className="btn btn-primary">Add</button>
+        <button className="btn btn-primary" onClick={updateModule}>
+                Update
+        </button>
+
+      </div>
+    )
+  }
+
+  // const modulesList = modules.filter((module) => module.course === courseId);
+  const [selectedModule, setSelectedModule] = useState(moduleList[0]);
     return (
       <div className="modules-list">
       <div className="top-buttons">
@@ -21,9 +73,11 @@ function ModuleList() {
         </select>
         <button className="red-button">+ Module</button>
         <FaEllipsisV className="ms-2 pad-ellipse" />
-      </div>  
-      <ul className="list-group wd-modules">
-        {modulesList.map((module) => (
+        </div>  
+        <AddNewModule />
+        <ul className="list-group wd-modules">     
+        {moduleList.filter((module) => module.course === courseId)
+          .map((module) => (
           <li
             className="list-group-item"
             onClick={() => setSelectedModule(module)}>
@@ -34,11 +88,11 @@ function ModuleList() {
                 <FaCheckCircle className="text-success" />
                 <FaPlusCircle className="ms-2" />
                 <FaEllipsisV className="ms-2" />
-              </span>
+                </span>
+                <div>{module.description}</div>
             </div>
-            {selectedModule._id === module._id && (
               <ul className="list-group">
-                {module.lessons?.map((lesson) => (
+                {module.lessons?.map((lesson: any) => (
                   <li className="list-group-item">
                     <FaEllipsisV className="me-2" />
                     {lesson.name}
@@ -46,10 +100,21 @@ function ModuleList() {
                       <FaCheckCircle className="text-success" />
                       <FaEllipsisV className="ms-2" />
                     </span>
+                    <div>{lesson.description}</div>
                   </li>
                 ))}
+                
               </ul>
-            )}
+            <div className="bottom-module-btns">
+            <button 
+                onClick={() => deleteModule(module._id)}>
+                Delete
+              </button>
+            <button
+              onClick={(event) => { setModule(module); }}>
+              Edit
+            </button>
+            </div>
           </li>
         ))}
         </ul>
